ab4b29fd8418b3fc68e1e0f10d1720138886ceb6
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-08-29 07:12:44 +0000

    eslint and prettier setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,9 +5,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text className="text-red-500">
-        Open up App folder to start working on your app!
-      </Text>
+      <Text className="text-red-500">Uber Clone</Text>
 
       <StatusBar style="auto" />
     </SafeAreaView>
