a076739a12cec19adad6acec1db39ca79b00a1a3
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-08-30 07:27:34 +0000

    user api fetch request created

---
+++ environment.d.ts
@@ -0,0 +1,14 @@
+// This file is needed to support autocomplete for process.env
+export {};
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      // clerk publishable key
+      EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
+
+      // postgres db url (neon db)
+      DATABASE_URL: string;
+    }
+  }
+}
