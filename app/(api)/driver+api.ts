320a650d1d3ac4b1804ab109b8187566aad49198
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-09-04 10:17:34 +0000

    drivers api endpoint created

---
+++ app/(api)/driver+api.ts
@@ -0,0 +1,15 @@
+import { neon } from "@neondatabase/serverless";
+
+export async function GET() {
+  try {
+    const sql = neon(process.env.DATABASE_URL!);
+
+    const response = await sql`SELECT * FROM drivers`;
+
+    return Response.json({ data: response });
+  } catch (error) {
+    console.log("[GET_DRIVERS]: ", error);
+
+    return Response.json({ error }, { status: 500 });
+  }
+}
